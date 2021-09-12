91d6e35ec06e8013d31ceb8c7b08e9401b0fb8d4
Jeremy S <JeremyDSpotts@gmail.com>
2021-09-11 23:45:57 -0500

    added routing to App.js with dummy pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
-import RelativePitch from './components/RelativePitch';
-import { Route, Link } from "react-router-dom";
-
+import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
+function Landing(){
+  return (
+    <div>
+      <h1>Landing</h1>
+    </div>
+  )
+}
+function RP(){
+  return (
+    <div>
+      <h1>Relative Pitch</h1>
+    </div>
+  )
+}
+function IP(){
+  return (
+    <div>
+      <h1>Interval Pitch</h1>
+    </div>
+  )
+}
+function CP(){
+  return (
+    <div>
+      <h1>Chord Pitch</h1>
+    </div>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          <RelativePitch/>
-        </a>
-      </header>
-    </div>
+    <Router>
+
+        {/* <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div> */}
+
+          <Switch>
+            <Route exact path="/">
+              <Landing/>
+            </Route>
+            <Route path="/RP">
+              <RP/>
+            </Route>
+            <Route path="/IP">
+              <IP/>
+            </Route>
+            <Route path="/CP">
+              <CP/>
+            </Route>
+          </Switch>
+    </Router>
   );
 }
 
